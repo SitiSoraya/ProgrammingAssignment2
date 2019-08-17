a1ef0be26d8674c3630cadf5b743cfa01ef714bb
SitiSoraya <49268801+SitiSoraya@users.noreply.github.com>
2019-08-17 13:38:57 +0800

    Update cachematrix.R
    Define getMatrix, setInvMatrix, GetInvMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,29 @@
 ## SitiSoraya
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
 ## This function creates matrix and returns the inverse matrix
 makeCacheMatrix <- function(x = matrix()) {
+        
         invMatrix <- NULL
+        
         setMatrix <- function(y){
                 x <<- y
                 invMatrix <<- NULL
         }
+        
+        getMatrix <- function(){
+                x
+        }
+        
+        setInvMatrix <- function(inv){
+                invMatrix <<- inv
+        }
+        
+        getInvMatrix <- function(){
+                invMatrix
+        }
+        
+        list(setMatrix = setMatrix, getMatrix = getMatrix, setInvMatrix = setInvMatrix, getInvMatrix = getInvMatrix)
 }
-
 
 ## Write a short comment describing this function
 
